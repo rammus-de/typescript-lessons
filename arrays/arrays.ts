499dc7690f7517f01bc5f6fc19cc4754016be27d
rammus-de <mmamirisacc@gmail.com>
2022-09-05 17:43:49 +0430

    section 4 - arrays

---
+++ arrays/arrays.ts
@@ -0,0 +1,29 @@
+const activeUsers: string[] = [];
+activeUsers.push("steven");
+activeUsers.push("tony");
+
+const ageList: number[] = [3, 25, 95];
+ageList[0] = 99;
+// ageList[0] = "asd"
+
+const bools: Array<boolean> = [];
+const bools2: boolean[] = [];
+
+type Point = {
+  x: number;
+  y: number;
+};
+
+const coords: Point[] = [];
+coords.push({ x: 23, y: 8 });
+coords.push({ x: 83, y: 98 });
+
+const board: string[][] = [
+  ["X", "O", "X"],
+  ["X", "O", "X"],
+  ["X", "O", "X"],
+  ["X", "O", "X"],
+];
+
+
+const demo:number[][][] = [[[1]],[[77]],[[9]]]
