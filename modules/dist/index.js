948d23f2905009ea9fe16f7544de7a8f6d468ba8
rammus-de <mmamirisacc@gmail.com>
2022-09-09 20:45:25 +0430

    section 15 - typescript modules

---
+++ modules/dist/index.js
@@ -0,0 +1,3 @@
+import { add, sample } from './utils.js';
+console.log(sample([23, 4, 5]));
+console.log(add(1, 2));
---
+++ modules/src/index.ts
@@ -0,0 +1,9 @@
+import {sample as randomSample}  from './utils.js'
+import type {Person } from  './types'
+import add from './utils'
+import {pi} from './utils'
+const sample = 1234
+const player1:Person = {email:"e@@.com",username:"mkdir"}
+console.log(randomSample([23,4,5]))
+console.log(add(1,2))
+console.log(pi)
